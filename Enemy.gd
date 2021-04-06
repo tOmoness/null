965e2b1ff6bcbdc7e5ad2433439cdbba0c9dbfb8
Tom Harts <thomas.harts@gmail.com>
2021-04-07 00:29:50 +0100

    Make enemy move to player.

--- Enemy.gd
+++ Enemy.gd
@@ -1,15 +1,6 @@
 extends KinematicBody2D
 
-enum DIRECTION { UP = -1, DOWN = 1 }
-
-var motion = Vector2.ZERO
-var direction = DIRECTION.UP
-
 func _physics_process(delta):
-	if is_on_ceiling():
-		direction = DIRECTION.DOWN
-	if is_on_floor():
-		direction = DIRECTION.UP
-	
-	motion.y += (300 * delta) * direction
-	motion = move_and_slide(motion.clamped(100), Vector2.UP)
+	$Sprite.flip_h = global_position > Global.Player.global_position
+	var direction = global_position.direction_to(Global.Player.global_position)
+	move_and_collide(direction * 10 * delta)
