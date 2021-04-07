883ed338088af1265e558d4af2f60fec281a3bfd
Tom Harts <thomas.harts@gmail.com>
2021-04-08 00:07:50 +0100

    Enemy dies when hit with bullet.

--- Enemy.gd
+++ Enemy.gd
@@ -4,3 +4,7 @@ func _physics_process(delta):
 	$Sprite.flip_h = global_position > Global.Player.global_position
 	var direction = global_position.direction_to(Global.Player.global_position)
 	move_and_collide(direction * 10 * delta)
+
+
+func _on_Hitbox_area_entered(area):
+	queue_free()
