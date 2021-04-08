cfbeecdeb855dacdb36d2f54eec81e2f9d7bf7d4
Tom Harts <thomas.harts@gmail.com>
2021-04-08 19:04:00 +0100

    Add end screen.

--- UI/SceneChanger.gd
+++ UI/SceneChanger.gd
@@ -3,9 +3,11 @@ extends CanvasLayer
 onready var black := $Control/ColorRect
 onready var anim := $AnimationPlayer
 
+var end_screen = preload("res://UI/EndScreen.tscn")
+
 func change_scene(target_scene: PackedScene):
 	anim.play("Fade")
 	yield(anim, "animation_finished")
-	get_tree().change_scene_to(target_scene)
+	get_tree().change_scene_to(target_scene if target_scene != null else end_screen)
 	anim.play_backwards("Fade")
 	yield(anim, "animation_finished")
